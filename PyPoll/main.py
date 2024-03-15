4df219092ee530ef568f88385aa223f70ba99dcc
Elizabeth Dashwood <dashwoodliz@gmail.com>
2024-03-15 21:23:09 +1100

    Addition of file totals

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,11 +9,27 @@ csvpath = os.path.join('.', 'Resources', 'election_data.csv')
 
 # Set steps to read csv file using the CSV module defined previously
 with open(csvpath) as csvfile:
-
-    # Specify the variable that holds the csv file contents and the delimiter for the CSV file
-    csvreader = csv.reader(csvfile, delimiter = ',')
+    
+ # Specify the variable that holds the csv file contents and the delimiter for the CSV file
+    csvreader = csv.reader(csvfile, delimiter =',')
     print (csvreader)
-
-    # Read the header row of the csv file and print using f-string
+    
+    # Read the header row of the csv file using f-string
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
+
+   # Count the number of data rows in the csv file
+    csvcount = len(list(csvreader))
+    # Reference: https://www.w3resource.com/python-exercises/modules/python-module-csv-exercise-2.php    
+    # Reference: https://docs.python.org/3/tutorial/datastructures.html
+    print("Total: " + str(csvcount))
+    
+   # Read each row of data after the header
+    #for row in csvreader:
+      #print(row)
+         
+               
+   # Print Report Title
+    print ("Election Results")
+    print ("----------------------------")
+    print ("Total Votes: " + str(csvcount))
